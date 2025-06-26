b2039b846921d0d74c86f23547fbd0da6fb8ee11
Umutoni <u.nada@alustudent.com>
2025-06-26 12:40:25 -0700

    commit 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 # Outer loop for the first digit (0 to 9)
 for i in range(10):
-    
+
     for j in range(10):
-        
+
         if i < j:
-            
+
             if i == 8 and j == 9:
-                
+
                 print("{}{}".format(i, j))
             else:
-            
+
                 print("{}{}, ".format(i, j), end="")
