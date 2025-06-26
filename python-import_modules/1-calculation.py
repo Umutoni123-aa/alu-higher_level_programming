4f63f96801e85317743ec2562f95171b68a3461f
Umutoni <u.nada@alustudent.com>
2025-06-26 14:53:33 -0700

    commit 1

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+
+    a = 10
+    b = 5
+
+    # Addition
+    print("{} + {} = {}".format(a, b, add(a, b)))
+
+    # Subtraction
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+
+    # Multiplication
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+
+    # Division
+    print("{} / {} = {}".format(a, b, div(a, b)))
