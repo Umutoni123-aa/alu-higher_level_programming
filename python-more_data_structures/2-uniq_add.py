ba5a215d1fbc675fd581e27b17736d76bdf47cfb
Umutoni <u.nada@alustudent.com>
2025-06-27 13:29:23 -0700

    data 2

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-# The use of  __import__
-uniq_add = __import__('2-uniq_add').uniq_add
-
-my_list = [1, 2, 3, 1, 4, 2, 5]
-result = uniq_add(my_list)
-print("Result: {:d}".format(result))
+def uniq_add(my_list=[]):
+    unique_elements = set(my_list)
+    total_unique_sum = sum(unique_elements)
+    return total_unique_sum
