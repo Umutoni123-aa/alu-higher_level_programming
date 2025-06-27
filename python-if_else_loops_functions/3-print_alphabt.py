bb4c922ae2b3d70bede31b212b9cb535f5bd933f
Umutoni <u.nada@alustudent.com>
2025-06-27 13:41:26 -0700

    commit 3

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 for i in range(ord('a'), ord('z') + 1):
-
     char_to_print = chr(i)
-    
     if char_to_print == 'q' or char_to_print == 'e':
         continue 
-    
     print("{}".format(char_to_print), end="")
