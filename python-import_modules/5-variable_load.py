b470b4e02994e72df4bdd137bd8dabdb01f235aa
Umutoni <u.nada@alustudent.com>
2025-06-27 09:42:52 -0700

    commit 5

--- python-import_modules/5-variable_load.py
+++ python-import_modules/5-variable_load.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-# This script imports a variable 'a' from 'variable_load_5.py'
-# and prints its value.
-
-# The __name__ == "__main__" block ensures this code only runs
-# when the script is executed directly, not when imported as a module.
-if __name__ == "__main__": 
-    a = __import__('variable_load_5').a
-
-    print(a) 
+if __name__ == "__main__":
+    from variable_load_5 import a
+    print(a)
