1fcbb704529f4ca899fe157dd7276beb50617243
Umutoni <u.nada@alustudent.com>
2025-06-27 09:15:40 -0700

    commit 0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-def add(a, b):
-    """My addition function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a + b
-    """
-    return (a + b)
+if __name__ == "__main__":
+    add = __import__('add_0').add
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
