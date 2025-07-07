db31e2c9d4a12789aac3cdeb27c71f5d9e40de08
Umutoni <u.nada@alustudent.com>
2025-07-07 04:28:31 -0700

    exceptions 2

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    elements_printed = 0
+    for i in range(x): 
+        try:
+           ValueError/TypeError.
+            print("{:d}".format(my_list[i]), end="")
+            elements_printed += 1 
+        except (ValueError, TypeError:
+        except IndexError:
+            break
+    print()
+    return elements_printed 
