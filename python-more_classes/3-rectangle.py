caa9f32ccedc749e9a728b4194f76bf30e3972e0
Umutoni <u.nada@alustudent.com>
2025-07-09 15:00:21 -0700

    more 3

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -19,7 +19,7 @@ class Rectangle:
     def width(self, value):
         """Setter method to set the width value"""
         if not isinstance(value, int):
-            raise TypeError("width must be an integer")
+            raise TypeError("The width must be integer")
         elif value < 0:
             raise ValueError("The width must be >= 0")
         self.__width = value
@@ -33,7 +33,7 @@ class Rectangle:
     def height(self, value):
         """Setter method to set the height value"""
         if not isinstance(value, int):
-            raise TypeError("The height must be integer")
+            raise TypeError("The height must be an integer")
         elif value < 0:
             raise ValueError("The height must be >= 0")
         self.__height = value
@@ -43,9 +43,23 @@ class Rectangle:
         return r_area
 
     def perimeter(self):
+        """Returns the perimeter of the rectangle"""
         r_perimeter = 2 * (self.__width + self.__height)
 
         if self.__width == 0 or self.__height == 0:
             return 0
         else:
             return r_perimeter
+
+    def __str__(self):
+        """Prints the rectangle using #"""
+        if self.__width == 0 or self.__height == 0:
+            return ''
+        else:
+            rect = ''
+            for i in range(self.__height):
+                for x in range(self.__width):
+                    rect = rect + '#'
+
+                rect += '\n'
+            return rect[:-1]
