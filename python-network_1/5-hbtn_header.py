e4d6bd54334b90f79b3508e3f08afbbc5a8149cd
Umutoni123-aa <u.nada@alustudent.com>
2025-07-28 18:39:54 +0000

    commits on python-network_1

---
+++ python-network_1/5-hbtn_header.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+a 5-hbtn_header.py
+"""
+
+
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    le = requests.get(url)
+    la = le.headers.get('X-Request-Id')
+    print(la)
