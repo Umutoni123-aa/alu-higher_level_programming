9a31267033e8b87ff7d527c448e354bb903d613e
Umutoni123-aa <u.nada@alustudent.com>
2025-10-02 07:34:28 -0700

    Make all JavaScript files executable

---
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// Defines a Rectangle class with width and height
+class Rectangle {
+  constructor(w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
